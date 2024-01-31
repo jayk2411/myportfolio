11e056a91b374b6d992b5b3322be16c753bea40c
jaykumar kakadiya <jaykakdiya2411@gmail.com>
2024-01-31 13:21:58 -0500

    fix route for starting page

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -12,6 +12,7 @@ const MainRouter = () => {
  <Layout/>
  <Routes>
  
+<Route exact path="/" element={<Home />} />
 <Route exact path="/home" element={<Home />} />
 <Route exact path="/about" element={<About />} />
 <Route exact path="/service" element={<Service />} />
