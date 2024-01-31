1bb501601ce1e0081eb49e29b18e613bc03222be
jaykumar kakadiya <jaykakdiya2411@gmail.com>
2024-01-31 13:54:48 -0500

    add popup for submitting the contact page and redirect to home

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,10 +1,19 @@
 //contat.jsx, JAYKUMAR KAKADIYA, 301430863, and 28/01/2024.
+import { useNavigate } from "react-router-dom";
 import '../src/contact.css'
 export default function Contact() {
+
+     let navigate = useNavigate();
+     const routeChange = (event) => {
+          event.preventDefault();
+          alert("Thank you for contacting us. We will get back to you as soon as possible.");
+          let path = '/';
+          navigate(path);
+     }
      return (
           <div>
                <p>Contact</p>
-               <form id="ffp">
+               <form id="ffp" onSubmit={routeChange}>
 
 
                     <label htmlFor="myFName"> *First Name: </label>
@@ -17,7 +26,7 @@ export default function Contact() {
                     <label htmlFor="myEmail"> *Email: </label>
                     <input type="email" id="myEmail" name="myEmail" required="required" /> <br /> <br />
 
-                    <input type="submit" className="submit" value="Submit" /> <br />
+                    <input type="submit" className="submit" value="Submit"/> <br />
 
 
                </form> <br></br>
